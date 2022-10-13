18894b8fa22f5f65cad0e53b6b1c8eda4456c568
jl-alcontin <106054681+jl-alcontin@users.noreply.github.com>
2022-10-12 21:12:41 -0500

    Update Skill.tsx
    image size

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -21,7 +21,7 @@ function Skill({ skill,directionLeft }: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src={urlFor(skill?.image).url()}
-        className="rounded-full border border-gray-500 object-cover w-12 h-12 sm:w-16 sm:h-16 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+        className="rounded-full border border-gray-500 object-cover w-12 h-12 sm:w-16 sm:h-16 md:w-24 md:h-24 lg:w-28 lg:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-12 h-12 sm:w-16 sm:h-16 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center w-full h-full absolute">

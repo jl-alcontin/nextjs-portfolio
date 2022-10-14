e02d9f2e643f13b90efecb0a99660fb5acc5689d
jl-alcontin <106054681+jl-alcontin@users.noreply.github.com>
2022-10-13 20:55:31 -0500

    Update Projects.tsx
    project list

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -16,7 +16,7 @@ function Projects({ projects }: Props) {
       className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
-        Projects
+        Project List
       </h3>
 
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">
